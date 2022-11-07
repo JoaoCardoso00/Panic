af1f8b0e2b136cf67105ab873962e361915d914a
JoaoCardoso00 <jvcq13@gmail.com>
2022-11-07 18:47:25 -0300

    added color styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-gray": "#202024",
+        "text-white": "#E1E1E6",
+        "text-gray": "#7C7C8A",
+        "foreground-gray": "#d9d9d90d",
+      },
+    },
   },
   plugins: [],
 };
