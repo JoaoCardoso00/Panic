ccbd08f7739f1546d9558109e91f1d0a35928912
JoaoCardoso00 <jvcq13@gmail.com>
2022-11-12 17:49:03 -0300

    added todo to light component

--- src/components/Light.tsx
+++ src/components/Light.tsx
@@ -1,4 +1,6 @@
 export function Light() {
+  //TODO: light doenst work on chrome
+
   return (
     <div className="light absolute left-2/4 top-[-100rem] ml-[-50rem] h-[100rem] w-[100rem] rounded-full" />
   );
